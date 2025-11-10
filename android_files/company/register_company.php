400c948c9781604af671b78a465631c646b3a9c3
Nivall <nivahmichael@gmail.com>
2025-11-10 18:19:40 +0300

    implemented backend company registration

--- android_files/company/register_company.php
+++ android_files/company/register_company.php
@@ -6,15 +6,19 @@ include '../../include/connections.php';
 
 if ($_SERVER['REQUEST_METHOD'] =='POST') {
 
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
+    $company_name = $_POST['company_name'];
+    $industry = $_POST['industry'];
+    $username = $_POST['username'];
+    $location = $_POST['location'];
+    $website = $_POST['website'];
+    $company_description = $_POST['company_description'];
     $username = $_POST['username'];
     $phoneNo = $_POST['phoneNo'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
 
-    if (empty($firstname) || empty($lastname) || empty($username) || empty($email) || empty($phoneNo) || empty($password)) {
+    if (empty($company_name) || empty($industry) || empty($username) || empty($location) || empty($phoneNo) || empty($password)) {
         $response["status"] = 0;
         $response["message"] = "Some details are missing ";
         echo json_encode($response);
@@ -24,37 +28,37 @@ if ($_SERVER['REQUEST_METHOD'] =='POST') {
 
         // check if username already exists
 
-        $select = "SELECT username FROM applicants WHERE username='$username'";
+        $select = "SELECT username FROM employers WHERE username='$username'";
         $query = mysqli_query($con, $select);
         if (mysqli_num_rows($query) > 0) {
 
             $response["status"] = 0;
-            $response["message"] = "Username is registered with another account";
+            $response["message"] = "Username is registered with another company";
             echo json_encode($response);
 
         } else {
 
-            $select = "SELECT phone FROM applicants WHERE phone='$phoneNo'";
+            $select = "SELECT contacts FROM employers WHERE contacts='$phoneNo'";
             $query = mysqli_query($con, $select);
             if (mysqli_num_rows($query) > 0) {
 
                 $response["status"] = 0;
-                $response["message"] = "Phone number is registered with another account";
+                $response["message"] = "Phone number is registered with another company";
                 echo json_encode($response);
 
             } else {
-                $select = "SELECT email FROM applicants WHERE email='$email'";
+                $select = "SELECT email_address FROM employers WHERE email_address='$email'";
                 $query = mysqli_query($con, $select);
                 if (mysqli_num_rows($query) > 0) {
 
                     $response["status"] = 0;
-                    $response["message"] = "Email is registered with another account";
+                    $response["message"] = "Email is registered with another company";
                     echo json_encode($response);
 
                 } else {
 
-                    $insert = "INSERT INTO applicants(first_name, last_name, username, phone, email,password)
-                VALUES ('$firstname','$lastname','$username','$phoneNo','$email','$password')";
+                    $insert = "INSERT INTO employers(company_name, username, contacts, email_address, industry,location, website, description)
+                VALUES ('$company_name','$username','$phoneNo','$email','$industry','$location', '$website','$company_description')";
                     if (mysqli_query($con, $insert)) {
 
                         $response["status"] = 1;
