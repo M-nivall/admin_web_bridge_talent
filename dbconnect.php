423928376931cf7a596b7af4aefc8c5c1027e0e8
nival <nivahmichael@gmail.com>
2026-05-31 15:01:40 +0300

    updated railway conn

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 // initializing variables
 $username = "";
 $email   = "";
