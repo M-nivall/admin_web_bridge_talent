450225761025fe1ec5f5f550ad833bbd317a1d52
nival <nivahmichael@gmail.com>
2026-07-02 23:41:22 +0300

    implemented railway db_connection

--- dbconnect.php
+++ dbconnect.php
@@ -12,13 +12,6 @@ $phone = "";
 $errors = array();
 
 /* ==============================
-   OLD LOCAL DATABASE (COMMENTED)
-   ==============================
-$db = mysqli_connect('localhost', 'root', '', 'bridge_talent');
-*/
-
-
-/* ==============================
    NEW RAILWAY DATABASE CONFIG
    ============================== */
 
@@ -47,11 +40,16 @@ if (isset($_POST['reg_user'])) {
   if (empty($username)) { array_push($errors, "Username cannot be empty"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
+
   if ($password_1 != $password_2) {
     array_push($errors, "The two passwords do not match");
   }
 
-  $user_check_query = "SELECT * FROM admin WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM admin
+                       WHERE username='$username'
+                       OR email='$email'
+                       LIMIT 1";
+
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
 
@@ -67,10 +65,13 @@ if (isset($_POST['reg_user'])) {
 
   if (count($errors) == 0) {
 
+    // Keeping your original behavior
     $password = md5($password_1);
 
-    $query = "INSERT INTO admin (username, email, phone, password) 
-              VALUES('$username', '$email', '$phone', '$password')";
+    $query = "INSERT INTO admin
+              (username, email, phone, password)
+              VALUES
+              ('$username', '$email', '$phone', '$password')";
 
     mysqli_query($db, $query);
 
@@ -78,6 +79,7 @@ if (isset($_POST['reg_user'])) {
     $_SESSION['success'] = "You are now logged in";
 
     header('location: index.php');
+    exit();
   }
 }
 
@@ -91,21 +93,30 @@ if (isset($_POST['login_user'])) {
   if (empty($username)) {
     array_push($errors, "Username is required");
   }
+
   if (empty($password)) {
     array_push($errors, "Password is required");
   }
 
   if (count($errors) == 0) {
 
-    $password = md5($password);
+    // EXACTLY like your old working code
+    $password = $password;
 
-    $query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+    $query = "SELECT * FROM admin
+              WHERE username='$username'
+              AND password='$password'";
+
     $results = mysqli_query($db, $query);
 
     if (mysqli_num_rows($results) == 1) {
+
       $_SESSION['username'] = $username;
       $_SESSION['success'] = "You are now logged in";
+
       header('location: index.php');
+      exit();
+
     } else {
       array_push($errors, "Wrong username AND/OR password combination");
     }
